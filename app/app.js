3ce93ec6140ff72469a61d454445ce3f853d615c
Josh <Tattsy@hotmail.co.uk>
2015-08-19 11:59:05 +0100

    text colour is based on temperature now

--- app/app.js
+++ app/app.js
@@ -7,20 +7,33 @@
 //   thermostat.temperature();
 //   });
 thermostat = new Thermostat;
-document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
-
+function value() {
+  document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+  document.getElementsByTagName("h1")[0].style.color = colour()
+};
+value()
+document.getElementsByTagName("h1")[0].style.color = colour()
 function increase() {
   thermostat.increaseBy(1)
-  document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+  value()
 };
 function decrease() {
   thermostat.decreaseBy(1)
-  document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+  value()
 };
 function reset() {
   thermostat.resetTemp()
-  document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+  value()
 };
 function psm() {
   thermostat.psmswitch()
+};
+function colour() {
+  if (thermostat.temperature < 18) {
+    return "green";
+  } else if (thermostat.temperature < 25) {
+    return "orange";
+  } else {
+    return "red";
+  };
 };
