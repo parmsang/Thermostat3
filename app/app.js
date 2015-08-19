b64e6273469a7db30aa460bc5e4ebd989b6ebfdb
squarebe <squarebe03@gmail.com>
2015-08-19 11:14:57 +0100

    we can increase the temperature setting

--- app/app.js
+++ app/app.js
@@ -7,4 +7,9 @@
 //   thermostat.temperature();
 //   });
 thermostat = new Thermostat;
-document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature
+document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+
+function increase() {
+  thermostat.increaseBy(1)
+  document.getElementsByTagName("h1")[0].innerHTML = thermostat.temperature + ' &deg;C'
+};
