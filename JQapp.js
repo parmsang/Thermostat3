33c15db46d69fa206b68fd008f2f990dbb5b8840
squarebe <squarebe03@gmail.com>
2015-08-19 14:26:07 +0100

    added colouring function

--- JQapp.js
+++ JQapp.js
@@ -1,17 +1,29 @@
 thermostat = new Thermostat;
 $(document).ready(function() {
+  function value() {
   $('h1').text(thermostat.temperature);
+    $('h1').css("color", function() {
+      if (thermostat.temperature < 18) {
+        return ("green");
+      } else if (thermostat.temperature < 25) {
+        return ("orange");
+      } else {
+        return ("red");
+      };
+    });
+  };
+  value()
   $('increasebutton').click(function() {
-      thermostat.increaseBy(1);
-      $('h1').text(thermostat.temperature);
+    thermostat.increaseBy(1);
+    value();
   });
   $('decreasebutton').click(function() {
     thermostat.decreaseBy(1);
-    $('h1').text(thermostat.temperature);
+    value()
   });
   $('resetbutton').click(function() {
     thermostat.resetTemp();
-    $('h1').text(thermostat.temperature);
+    value()
   });
   $('PSMbutton').click(function() {
     thermostat.psmswitch();
